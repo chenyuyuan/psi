36605abc8b01d46c583b37ca8ca0eb3cf0e94390
randylltarly <1695215206@qq.com>
2018-02-27 23:34:50 +0800

    webstorm

--- server/app/models.py
+++ server/app/models.py
@@ -1,24 +1,5 @@
 from app import db
 
-class plan(db.Model):
-    __tablename__ = 'plan'
-    id = db.Column(primary_key=True)
-    name = db.Column()
-    key = db.Column()
-    date = db.Column()
-    wechat = db.Column()
-    activity_one = db.Column()
-    activity_two = db.Column()
-    activity_three = db.Column()
-    activity_four = db.Column()
-    activity_five = db.Column()
-    activity_six = db.Column()
-    hotel = db.Column()
-    restaurant_one = db.Column()
-    restaurant_two = db.Column()
-    restaurant_three = db.Column()
-    restaurant_four = db.Column()
-
 class user(db.Model):
     __tablename__='user'
     id=db.Column(primary_key=True)
