7c06c5de2dc7b1bd9920281c543aec9d260898ab
randylltarly <1695215206@qq.com>
2018-02-28 14:16:22 +0800

    webstorm

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -10,4 +10,4 @@ api = Api(app)
 db = SQLAlchemy(app)
 app.secret_key = '~\xc8\xc6\xe0\xf3,\x98O\xa8z4\xfb=\rNd'
 
-from app import models, router
+from app import models, router,url
--- server/app/url/logout.py
+++
@@ -1,7 +0,0 @@
-from app import db, models
-from flask_restful import Resource, Api, reqparse, abort
-from flask import Flask, session, request
-import flask
-
-app = Flask(__name__)
-app.secret_key = '~\xc8\xc6\xe0\xf3,\x98O\xa8z4\xfb=\rNd'
