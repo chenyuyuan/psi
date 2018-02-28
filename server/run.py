af9fcb4f2d0f62477189db644a03f0997d0686e5
randylltarly <1695215206@qq.com>
2018-03-01 00:03:09 +0800

    webstorm

--- server/run.py
+++ server/run.py
@@ -2,6 +2,9 @@ from app import app
 
 
 from app.url.bootstrap import bo
+from app.url.upload import upload
+
+app.register_blueprint(upload)
 app.register_blueprint(bo,url_prefix='/bb')
 
 if __name__ == '__main__':
