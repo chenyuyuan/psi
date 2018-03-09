52e311d363d3dadb3db36fffa0b40a2d1ee16509
randylltarly <1695215206@qq.com>
2018-03-10 00:25:46 +0800

    Pycharm topic star

--- server/app/url/topic.py
+++ server/app/url/topic.py
@@ -1,11 +1,10 @@
 from flask import Flask, session, request
 import time,datetime
 from app import db, models,app
-from flask_restful import Resource, Api, reqparse, abort
 from flask import Flask, session, request,render_template,url_for
-from flask_bootstrap import Bootstrap
 from flask import Blueprint
-
+import json
+import time
 
 btopic=Blueprint('btopic', __name__)
 
@@ -17,7 +16,7 @@ def test(topicid):
     answer = list()
     answered=[]
     answer = db.session.execute(sql)
-    uid = 1#session['uid']
+    uid =session['uid']
     user=models.user.query.filter_by(id=uid).first()
     users=models.user.query.all()
     count=0
@@ -41,15 +40,54 @@ def test(topicid):
                 break
     print(answered)
     answered=answered[::-1]
-    for i in range(0,len(answered)):
-        print(answered[i]['id'])
-    for key in answered:
-        print(key['uid'])
+    # for i in range(0,len(answered)):
+    #     print(answered[i]['id'])
+    # for key in answered:
+    #     print(key['uid'])
     for key in answered:
         with open("C://Users//梅西//Desktop//forserver//answer//"+str(key['id'])+".txt","r+") as f:
             h=f.read()
         answered[count]["content"]=h
         count=count+1
-    # for key in answercontent:
-    #      print('qq'+key+'\n')
-    return render_template('/topic.html', title='话题',uid=uid,users=users,topicid=topicid,thistopic=thistopic,answered=answered,answercontent=answercontent)
+    topicstar = models.topicStar.query.filter_by(uid=uid,topicid=topicid).first()
+    thisstar=False
+    if topicstar:
+        thisstar=True
+    print(thisstar)
+    print(str(uid)+"+"+str(topicid))
+    return render_template('/topic.html', title='话题',thisstar=thisstar,uid=uid,users=users,topicid=topicid,thistopic=thistopic,answered=answered,answercontent=answercontent)
+
+@btopic.route('/topic/star',methods=['GET','POST'])
+def staringtopic():
+    req=request.get_json()
+    flag=req["flag"]
+    topicid=int(req["topicid"])
+    #flag=1进行关注话题
+    topictitle=models.topic.query.filter_by(id=topicid).first()
+    title=topictitle.topic
+    alltopicstar=models.topicStar.query.all()
+    maxid=alltopicstar[len(alltopicstar)-1].id+1
+    uid=session['uid']
+    if flag=='1':
+        topicstar=models.topicStar()
+        topicstar.id=maxid
+        topicstar.uid=uid
+        topicstar.topicid=topicid
+        topicstar.title=title
+        db.session.add(topicstar)
+        db.session.commit()
+        thistopic=models.topic.query.filter_by(id=topicid).first()
+        thistopic.starCount=thistopic.starCount+1
+        db.session.commit()
+        return json.dumps({"msg":"starok"})
+    elif flag=="0":
+        thistopic=models.topic.query.filter_by(id=topicid).first()
+        thistopic.starCount=thistopic.starCount-1
+        topicstar=models.topicStar.query.filter_by(uid=uid,topicid=topicid).first()
+        if topicstar:
+            topicstar.uid = 0
+        db.session.commit()
+        return json.dumps({"msg":"cancelok"})
+
+
+
