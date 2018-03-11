7478808e9118072309d51c435d32362f1585e99f
randylltarly <1695215206@qq.com>
2018-03-11 17:26:48 +0800

    Pycharm mypage

--- server/app/url/people.py
+++ server/app/url/people.py
@@ -14,11 +14,29 @@ def people():
     uid=session["uid"]
     users=models.user.query.all()
     user={}
+    question=[]
     for key in users:
         if key.id==uid:
             user=key
-    question=models.question.query.all()
-    return render_template('/pageofmine.html', title='我的主页', users=users,user=user)
+    questions=models.question.query.all()
+    for key in question:
+        if key.uid==uid:
+            flag="0"
+            if models.queLike.query.filter_by(uid=uid, queid=key.id).first():
+                print("查到")
+                flag = "1"
+            question.append({
+                "id":key.id,
+                "question":key.question,
+                "like":key.like,
+                "ilike":flag,
+                "ansCount":key.ansCount,
+                "time":key.time,
+                "reltopicid":key.reltopicid,
+                "followerCount":key.followerCount,
+                "queContent":key.queContent
+            })
+    return render_template('/pageofmine.html', title='我的主页',type="question", users=users,user=user,question=question)
 @bpeople.route('/people/<uid>/ask')
 def others(uid):
     # myid=session["uid"]
